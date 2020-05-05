01d2945899e638d6ab22ce630f706b4bf3b3c5e7
Dhanabalanvb <manisanker.vb@gmail.com>
2020-05-05 19:35:13 +0530

    Set configuration to different module

--- src/actions/leadactions.rs
+++ src/actions/leadactions.rs
@@ -1,7 +1,17 @@
-use actix_web::{ Responder, get, HttpRequest};
+use actix_web::{ web,Responder, get, HttpRequest};
 use crate::models::lead::*;
 
 #[get("/leadactions/getlead")]
 pub async fn get_lead(_req:HttpRequest) -> impl Responder{
     LeadModel::new(1, "Sample Lead".to_string())
+}
+
+#[get("/leadactions/getlead2")]
+pub async fn get_lead2(_req:HttpRequest) -> impl Responder{
+    LeadModel::new(2, "Sample Lead2".to_string())
+}
+
+pub fn config(cfg:&mut web::ServiceConfig){
+    cfg.service(get_lead);
+    cfg.service(get_lead2);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ extern crate actix_web;
 mod models;
 mod actions;
 
-use actix_web::{ App,  HttpServer };
+use actix_web::{ web, App,  HttpServer };
 use actions::leadactions::*;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(get_lead)
+        App::new()        
+            //.service(get_lead)
+            .service(web::scope("/lead").configure(config))
     })
     .bind("127.0.0.1:8088")?
     .run()
